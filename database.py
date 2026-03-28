23a16ff604433fc49090200eaf67e7f0c16121d4
Leticia Bi <leticiabi@LeticiadeMacBook-Air.local>
2026-03-28 11:58:13 +0100

    debug: print DB_URL

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DB_URL", "postgresql://localhost/taskmanager")
+print(f"DB_URL: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
