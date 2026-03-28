21ae249ce2e579d958c53c8d4e246ff2f543735a
Leticia Bi <leticiabi@LeticiadeMacBook-Air.local>
2026-03-28 09:19:08 +0100

    feat: add PostgreSQL database with SQLAlchemy

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-tasks = []
-
-class Task(BaseModel):
+class TaskSchema(BaseModel):
     title: str
     done: bool = False
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def hello():
     return {"message": "Hello 42!"}
 
 @app.get("/tasks")
-def get_tasks():
-    return tasks
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
 
 @app.post("/tasks")
-def create_task(task: Task):
-    tasks.append(task)
-    return task
+def create_task(task: TaskSchema, db: Session = Depends(get_db)):
+    db_task = models.Task(title=task.title, done=task.done)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
 
 @app.delete("/tasks/{id}")
-def delete_task(id: int):
-    tasks.pop(id)
+def delete_task(id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == id).first()
+    db.delete(task)
+    db.commit()
     return {"message": f"Task {id} deleted"}
 
 @app.put("/tasks/{id}")
-def update_task(id: int, updated_task: Task):
-    tasks[id] = updated_task
-    return updated_task
+def update_task(id: int, updated_task: TaskSchema, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == id).first()
+    task.title = updated_task.title
+    task.done = updated_task.done
+    db.commit()
+    db.refresh(task)
+    return task
---
+++ models.py
@@ -0,0 +1,9 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    done = Column(Boolean, default=False)
